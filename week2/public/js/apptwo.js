121c5690cfbb3b9c13161cb416a45e0a26f9431a
MitchGoudkuil <mitch_goudkuil@hotmail.com>
2019-02-11 19:52:46 +0100

    Added team container and imagebuttons

--- week2/public/js/apptwo.js
+++ week2/public/js/apptwo.js
@@ -3,20 +3,31 @@ import { renderData } from './modules/renderData.js';
 import { switchImage } from './modules/switchImage.js';
 
 // All button variables
-let loadMoreButton = document.querySelector('.loadmore')
-let shinyButton = document.querySelector('.shiny-button')
+let loadMoreButton = document.querySelector('.loadmore');
+let shinyButton = document.querySelector('.shiny-button');
+let teamButton = document.querySelector('.team-button');
+
+let teamContainer = document.querySelector('.team-container');
+
 let searchAmount = 0;
 
 apiCall().then(res => {
   renderData(res)
 })
 
-loadMoreButton.addEventListener("click", function(){
+// toggle for team container
+teamButton.addEventListener("click", function(){
+  teamContainer.classList.toggle('show')
+})
+
+// Loads 20 more pokemon to the viewport
+loadMoreButton.addEventListener('click', function(){
   apiCall(20).then(res => {
     renderData(res)
   })
 })
 
-shinyButton.addEventListener("click", function(){
+// switch function that changes images to shine one's
+shinyButton.addEventListener('click', function(){
 switchImage()
 })
